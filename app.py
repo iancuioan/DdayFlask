1e393eb5c74723333c8209fefda81e467b6e442e
Iancu Ioan <iancuioan897@yahoo.ro>
2022-06-23 22:31:18 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+from datetime import datetime, date
+
+azi = date.today()
+
+app = Flask(__name__)
+app.secret_key = "asecretkey"
+
+@app.route('/', methods=['POST','GET'])
+def home():
+    name = request.form.get('name')
+    if name != None:
+    	return render_template('bday.html', name=name, azi=azi)
+	
+    return render_template("home.html")
+
+
+
+def start_ngrok():
+	#from flask_ngrok import run_with_ngrok#pip install flask-ngrok
+    from pyngrok import ngrok
+    url = ngrok.connect(5000)
+    print('ngrok url = ', url)
+#start_ngrok()
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
